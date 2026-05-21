54bd1c104195fb9b6baaa281e8d86fec0bcfdcba
eddy12597@163.com <eddy12597@163.com>
2026-05-21 08:33:45 +0800

    updated Path handling in install_win.py

--- install_win.py
+++ install_win.py
@@ -1,4 +1,6 @@
 import subprocess
 import shutil
+from pathlib import Path
+
 subprocess.run(["pyinstaller", "--onefile", "main.py"])
-shutil.move("./dist/main.exe", "./dist/mcpm.exe")
+shutil.move(Path(__name__).parent / "dist" / "main.exe", Path(__name__).parent / "dist" / "mcpm.exe")
